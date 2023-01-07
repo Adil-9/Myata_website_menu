f65cb02ebd06571868a4c44ff75fb6990813b87f
HoleHorse <tamirlankulzhanov@gmail.com>
2023-01-07 20:25:44 +0600

    serve static

--- main.go
+++ main.go
@@ -9,5 +9,6 @@ import(
 func main() {
 	http.HandleFunc("/", render.Homepage)
 	fmt.Println("Server running on: http://localhost:8080")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
