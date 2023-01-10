e075c684ffc769bea19b6fa8de0b17ee92a13bd8
HoleHorse <tamirlankulzhanov@gmail.com>
2023-01-11 00:15:07 +0600

    s

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,27 @@
-function openMenu(event) {
+function openMenu(event, menuName) {
     const tablinks = document.getElementsByClassName("tablink");
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].classList.remove("myata_bg");
     }
     event.target.classList.add("myata_bg")
+    const menus = document.getElementsByClassName("menu")
+    for (var i = 0; i < menus.length; i++) {
+        menus[i].style.display = "none"
+    }
+    document.getElementById(menuName).style.display = "block"
 }
 document.getElementById("myLink").click();
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    var menu = document.getElementById("menu")
-    if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
-        menu.classList.add("top")
-    } else {
-        menu.classList.remove("top")
-    }
-}   
+// function scrollFunction() {
+//     var menu = document.getElementById("menu")
+//     var empty = document.getElementById("empty")
+//     if (document.body.scrollTop > screen.height*1.235 || document.documentElement.scrollTop > screen.height*1.235) {
+//         menu.classList.add("top")
+//         empty.style.display = "block"
+//     } else {
+//         menu.classList.remove("top")
+//         empty.style.display = "none"
+//     }
+// }   
