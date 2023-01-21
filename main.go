ad81bbcb55cfb51727e34f35a762fad3306ce112
HoleHorse <tamirlankulzhanov@gmail.com>
2023-01-22 02:46:47 +0600

    s

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	http.HandleFunc("/", handlers.HomePage)
 	fmt.Println("Server running on: http://localhost:3000")
-	http.Handle("/static/", http.FileServer(http.Dir("static")))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":3000", nil)
 }
