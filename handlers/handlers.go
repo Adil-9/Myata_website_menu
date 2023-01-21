5329abec6dd097c57f8b29dcc325c397848dbee7
HoleHorse <tamirlankulzhanov@gmail.com>
2023-01-22 00:49:01 +0600

    dynamic category

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,11 +11,45 @@ type Item struct {
 	Dish        string
 	Description string
 	Price       int
+	Category    string
 }
 
 type Items struct {
 	Category string
 	Dishes   []Item
+}
+
+var data []Item = []Item{
+	{1, "Ceasar", "anchovies, olive oil, lemon juice, egg, and Parmesan cheese, garnished with croutons", 1750, "Salads"},
+	{2, "Caprese", "Sliced mozzarella. Sliced tomatoes. Sweet basil. Olive oil. Salt.", 1250, "Salads"},
+	{3, "Cobb", "Chopped salad greens, tomato, crispy bacon, chicken breast, hard-boiled eggs, avocado, chives, Roquefort cheese and red wine vinaigrette.", 950, "Salads"},
+	{4, "Margarita", "tomatoes, mozzarella cheese, garlic, fresh basil, and extra-virgin olive oil", 2950, "Pizzas"},
+	{5, "Sicilian", "dough topped with mozzarella cheese and tomato sauce.", 2350, "Pizzas"},
+	{6, "Quattro formaggi", "topped with a combination of four kinds of cheese", 3150, "Pizzas"},
+	{7, "Gazpacho", "tomatoes, garlic, onions, pepper and olive oil", 1750, "Soups"},
+	{8, "Tom Yum", "several spices and herbs, including lemongrass, galangal and kaffir lime leaves.", 2150, "Soups"},
+	{9, "Ramen", "broth based on chicken, seasoned with taré and served with pasta", 1450, "Soups"},
+	{10, "Bolognese", "spaghetti and a sauce made of minced beef, tomatoes, onion, bacon, red wine and herbs", 2390, "Pasta"},
+	{11, "Fettuccine Alfredo", "fettuccine (flat pasta ribbons) tossed with parmesan cheese and butter.", 2290, "Pasta"},
+	{12, "Carbonara", "spaghetti with a cream-based sauce with ham or pancetta", 2490, "Pasta"},
+}
+
+func getData() []Items {
+	items := make([]Items, 5)
+	for i := 0; i < len(data); i++ {
+		for j := i; j > 0 && data[j-1].Category > data[j].Category; j-- {
+			data[j], data[j-1] = data[j-1], data[j]
+		}
+	}
+	j := 0
+	for i := 0; i < len(data); i++ {
+		if data[i].Category != items[j].Category {
+			j++
+		}
+		items[j].Category = data[i].Category
+		items[j].Dishes = append(items[j].Dishes, Item{data[i].Id, data[i].Dish, data[i].Description, data[i].Price, data[i].Category})
+	}
+	return items[1:]
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
@@ -32,32 +66,7 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 		errorHandler(w, r, http.StatusInternalServerError)
 		return
 	}
-	salads := []Item {
-		{1, "Ceasar", "anchovies, olive oil, lemon juice, egg, and Parmesan cheese, garnished with croutons", 1750},
-		{2, "Caprese", "Sliced mozzarella. Sliced tomatoes. Sweet basil. Olive oil. Salt.", 1250},
-		{3, "Cobb", "Chopped salad greens, tomato, crispy bacon, chicken breast, hard-boiled eggs, avocado, chives, Roquefort cheese and red wine vinaigrette.", 950},
-	}
-	pizzas := []Item {
-		{4, "Margarita", "tomatoes, mozzarella cheese, garlic, fresh basil, and extra-virgin olive oil", 2950},
-		{5, "Sicilian", "dough topped with mozzarella cheese and tomato sauce.", 2350},
-		{6, "Quattro formaggi", "topped with a combination of four kinds of cheese", 3150},
-	}
-	soups := []Item {
-		{7, "Gazpacho", "tomatoes, garlic, onions, pepper and olive oil", 1750},
-		{8, "Tom Yum", "several spices and herbs, including lemongrass, galangal and kaffir lime leaves.", 2150},
-		{9, "Ramen", "broth based on chicken, seasoned with taré and served with pasta", 1450},
-	}
-	pasta := []Item {
-		{10, "Bolognese", "spaghetti and a sauce made of minced beef, tomatoes, onion, bacon, red wine and herbs", 2390},
-		{11, "Fettuccine Alfredo", "fettuccine (flat pasta ribbons) tossed with parmesan cheese and butter.", 2290},
-		{12, "Carbonara", "spaghetti with a cream-based sauce with ham or pancetta", 2490},
-	}
-	arr := []Items{
-		{"Salads", salads},
-		{"Pizzas", pizzas},
-		{"Soups", soups},
-		{"Pasta", pasta},
-	}
+	arr := getData()
 	if err := tmpl.Execute(w, arr); err != nil {
 		errorHandler(w, r, http.StatusInternalServerError)
 		return
